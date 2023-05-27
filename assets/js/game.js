9f26ed0de5dd5444f6f238e0bf0cb0a322ff956c
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-05-27 19:28:13 +0200

    adding and removing clickable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,33 @@
 "use strict";
 
 let _moveSequence = [];
-let _gameStarted = false;
 
 function handleButtonClick(e) {
     e.preventDefault();
     const $button = e.target;
-    $button.id === 'start-button' ? handleStartClick() : handleColorClick($button);
+    $button.id === 'start-button' ? handleStartClick($button) : handleColorClick($button);
 }
 
-function handleStartClick() {
-    return -1;
+function handleStartClick($button) {
+    if (!$button.classList.contains('clickable')) {
+        return;
+    }
+    $button.classList.remove('clickable');
 }
 
 function handleColorClick($button) {
+    if (!$button.classList.contains('clickable')) {
+        return;
+    }
     const clickedColor = $button.id.split('-')[0];
+}
+
+function addClickableToColors() {
+    document.querySelectorAll("#green-button, #blue-button, #red-button, #yellow-button")
+        .forEach($button => $button.classList.add('clickable'));
+}
+
+function removeClickableFromColors() {
+    document.querySelectorAll("#green-button, #blue-button, #red-button, #yellow-button")
+        .forEach($button => $button.classList.remove('clickable'));
 }
